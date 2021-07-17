ab37d9b2f73225e647f72cccfcf765bcb8d73181
Joseph Vega <jcvb0206@gmail.com>
2021-07-17 17:01:25 -0500

    Creación de la pestaña Divisiones, filtrado y ordenado de las columnas en proceso

--- src/App.js
+++ src/App.js
@@ -1,136 +1,122 @@
-import { Layout } from 'antd';
-// import { Layout, Tabs, Button, Pagination, Radio, Input, Select, Table } from 'antd';
-// import {
-//   PlusOutlined,
-//   DownloadOutlined,
-//   UploadOutlined,
-// } from '@ant-design/icons';
+import {
+  Layout,
+  Typography,
+  Tabs,
+  Space,
+  Button,
+  Radio,
+  Input,
+  Select,
+  Table,
+} from 'antd';
+import {
+  PlusOutlined,
+  DownloadOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
 import Header from './components/Header';
 import 'antd/dist/antd.less';
+import data from './assets/data.json';
 
-// const columns = [
-//   {
-//     title: 'División',
-//     dataIndex: 'name',
-//     key: 'name',
-//     filters: [
-//       { text: 'Joe', value: 'Joe' },
-//       { text: 'Jim', value: 'Jim' },
-//     ],
-//   },
-//   {
-//     title: 'División Superior',
-//     dataIndex: 'age',
-//     key: 'age',
-//   },
-//   {
-//     title: 'Address',
-//     dataIndex: 'address',
-//     key: 'address',
-//     filters: [
-//       { text: 'London', value: 'London' },
-//       { text: 'New York', value: 'New York' },
-//     ],
-//     ellipsis: true,
-//   },
-//   {
-//     title: 'Colaboradores',
-//     dataIndex: 'colaboradores',
-//     key: 'colaboradores',
-//     filters: [
-//       { text: 'London', value: 'London' },
-//       { text: 'New York', value: 'New York' },
-//     ],
-//   },
-//   {
-//     title: 'Nivel',
-//     dataIndex: 'nivel',
-//     key: 'nivel',
-//     filters: [
-//       { text: 'London', value: 'London' },
-//       { text: 'New York', value: 'New York' },
-//     ],
-//   },
-//   {
-//     title: 'Subdivisiones',
-//     dataIndex: 'subdivisiones',
-//     key: 'subdivisiones',
-//     filters: [
-//       { text: 'London', value: 'London' },
-//       { text: 'New York', value: 'New York' },
-//     ],
-//   },
-//   {
-//     title: 'Embajadores',
-//     dataIndex: 'embajadores',
-//     key: 'embajadores',
-//     filters: [
-//       { text: 'London', value: 'London' },
-//       { text: 'New York', value: 'New York' },
-//     ],
-//   },
-// ];
+const columns = [
+  {
+    title: 'División',
+    dataIndex: 'division',
+    key: 'division',
+    filters: [
+      { text: 'CEO ', value: 'CEO' },
+      { text: 'Mandu-Corp', value: 'Mandu-Corp' },
+      { text: 'Growth', value: 'Growth' },
+      { text: 'Strategy', value: 'Strategy' },
+    ],
+    onFilter: (value, record) => record.division.indexOf(value) === 0,
+    sorter: (a, b) => a.division.length - b.division.length,
+  },
+  {
+    title: 'División Superior',
+    dataIndex: 'division-sup',
+    key: 'division-sup',
+    filters: [
+      { text: 'London', value: 'London' },
+      { text: 'New York', value: 'New York' },
+    ],
+  },
+  {
+    title: 'Colaboradores',
+    dataIndex: 'colaboradores',
+    key: 'colaboradores',
+  },
+  {
+    title: 'Nivel',
+    dataIndex: 'nivel',
+    key: 'nivel',
+    filters: [
+      { text: '1', value: 1 },
+      { text: '2', value: 2 },
+      { text: '3', value: 3 },
+      { text: '4', value: 4 },
+      { text: '5', value: 5 },
+    ],
+    onFilter: (value, record) => record.nivel === value,
+    sorter: (a, b) => a.nivel - b.nivel,
+  },
+  {
+    title: 'Subdivisiones',
+    dataIndex: 'subdivisiones',
+    key: 'subdivisiones',
+    filters: [
+      { text: 'London', value: 'London' },
+      { text: 'New York', value: 'New York' },
+    ],
+  },
+  {
+    title: 'Embajadores',
+    dataIndex: 'embajadores',
+    key: 'embajadores',
+  },
+];
 
-// const data = [
-//   {
-//     key: '1',
-//     name: 'John Brown',
-//     age: 32,
-//     address: 'New York No. 1 Lake Park',
-//   },
-//   {
-//     key: '2',
-//     name: 'Jim Green',
-//     age: 42,
-//     address: 'London No. 1 Lake Park',
-//   },
-//   {
-//     key: '3',
-//     name: 'Joe Black',
-//     age: 32,
-//     address: 'Sidney No. 1 Lake Park',
-//   },
-//   {
-//     key: '4',
-//     name: 'Jim Red',
-//     age: 32,
-//     address: 'London No. 2 Lake Park',
-//   },
-// ];
-
-// const rowSelection = {};
+const rowSelection = {};
 
 function App() {
   return (
     <Layout>
       <Header />
-      {/* <div>
-        <Button type="primary" icon={<PlusOutlined />} size={'large'} />
-        <Button type="primary" icon={<UploadOutlined />} size={'large'} />
-        <Button type="primary" icon={<DownloadOutlined />} size={'large'} />
-      </div>
-      <Tabs defaultActiveKey="1">
-        <Tabs.TabPane tab="Divisiones" key="1">
-          <section>
-            <Radio.Group value={'default'}>
-              <Radio.Button value="listado">Listado</Radio.Button>
-              <Radio.Button value="arbol">Árbol</Radio.Button>
-            </Radio.Group>
-            <Select placeholder="Columnas" optionFilterProp="children"></Select>
-            <Input.Search placeholder="Buscar" style={{ width: 200 }} />
-            <Table
-              rowSelection={rowSelection}
-              columns={columns}
-              dataSource={data}
-              bordered
-            />
-            <Pagination defaultCurrent={1} total={50} />
-          </section>
-        </Tabs.TabPane>
-        <Tabs.TabPane tab="Colaboradores" key="2">
-          <p>Otro Panel</p>
-        </Tabs.TabPane>
-      </Tabs> */}
+      <Layout.Content style={{ backgroundColor: 'white', padding: '0 25px' }}>
+        <Typography.Title level={2}>Organización</Typography.Title>
+        <div>
+          <Button type="primary" icon={<PlusOutlined />} size={'large'} />
+          <Button type="primary" icon={<UploadOutlined />} size={'large'} />
+          <Button type="primary" icon={<DownloadOutlined />} size={'large'} />
+        </div>
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="Divisiones" key="1">
+            <section>
+              <Space>
+                <Radio.Group value={'listado'}>
+                  <Radio.Button value="listado">Listado</Radio.Button>
+                  <Radio.Button value="arbol">Árbol</Radio.Button>
+                </Radio.Group>
+                <Select
+                  placeholder="Columnas"
+                  optionFilterProp="children"
+                ></Select>
+                <Input.Search placeholder="Buscar" style={{ width: 200 }} />
+              </Space>
+              <Table
+                rowSelection={rowSelection}
+                columns={columns}
+                dataSource={data}
+                pagination={{ current: 1, pageSize: 12 }}
+                bordered
+              />
+            </section>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Colaboradores" key="2">
+            <p>Otro Panel</p>
+          </Tabs.TabPane>
+        </Tabs>
+      </Layout.Content>
     </Layout>
   );
 }
